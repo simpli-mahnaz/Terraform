567a2e79da29203fbdbd68681ec3ead8979e4ab0
simpli-mahnaz <129036296+simpli-mahnaz@users.noreply.github.com>
2026-05-16 01:06:33 +0530

    Fix resource group reference in storage account

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "azurerm_resource_group" "RG1" {
 resource "azurerm_storage_account" "SA" {
     for_each = var.sa_name
   name = each.value.name
-  resource_group_name = azurerm_resource_group.RG1["uat"].name
+  resource_group_name = azurerm_resource_group.RG1[each.key].name
   location = each.value.location
   account_tier = each.value.account_tier
   account_replication_type =each.value.account_replication_type
