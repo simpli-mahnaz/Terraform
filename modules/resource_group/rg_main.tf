47faac82ae83f342e74fac093203721948d728ac
simpli-mahnaz <129036296+simpli-mahnaz@users.noreply.github.com>
2026-05-16 11:13:12 +0530

    Update rg_main.tf

--- modules/resource_group/rg_main.tf
+++ modules/resource_group/rg_main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_resource_group" "label"{
-  for_each=toset(var.rg_name)
+  for_each=var.rg_name
   name=each.value.name
   location=each.value.location
   managed_by=each.value.managed_by
