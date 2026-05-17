10cb94d53e1825f4b33f4647d6ee1e57903ec8d4
simpli-mahnaz <129036296+simpli-mahnaz@users.noreply.github.com>
2026-05-17 15:24:27 +0530

    Update main.tf

--- modules/implicit_dependency_common_var/main.tf
+++ modules/implicit_dependency_common_var/main.tf
@@ -1,1 +1,17 @@
+resource "azurerm_resource_group" "RGs" {
+    for_each = var.dev_env
+    name = each.value.rg_name
+    location = each.value.rg_location
+    managed_by = each.value.rg_managed_by
+  
+}
 
+resource "azurerm_storage_account" "SA" {
+    for_each = var.dev_env
+    name = each.value.sa_name
+    resource_group_name = azurerm_resource_group.RGs[each.key].name
+    location = azurerm_resource_group.RGs[each.key].location
+    account_tier = each.value.account_tier
+    account_replication_type = each.value.account_replication_type
+  
+}
